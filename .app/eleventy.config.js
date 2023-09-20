f19a85a93d9708444e142b5a378238b9a34b0cf8
David Benque <david.benque@xwiki.com>
2023-09-20 12:01:52 +0100

    Add support for Markdown footnotes

--- .app/eleventy.config.js
+++ .app/eleventy.config.js
@@ -17,6 +17,22 @@ module.exports = (function(eleventyConfig) {
   //   watch: ["dist/app.js", "dist/app.*.css"],
   // });
 
+  // footnotes
+  // https://www.alpower.com/tutorials/configuring-footnotes-with-eleventy/
+  // set markdown footnote processor
+  let markdownIt = require("markdown-it");
+  let markdownItFootnote = require("markdown-it-footnote");
+
+  let options = {
+    html: true, // Enable HTML tags in source
+    breaks: true,  // Convert '\n' in paragraphs into <br>
+    linkify: true // Autoconvert URL-like text to links
+  };
+  // configure the library with options
+  let markdownLib =  markdownIt(options).use(markdownItFootnote);
+  // set the library to process markdown files
+  eleventyConfig.setLibrary("md", markdownLib);
+
   eleventyConfig.addFilter('sortByOrder', function(values) {
     return values.sort((a, b) => Math.sign(a.data.order - b.data.order));
   })
